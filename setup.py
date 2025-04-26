31bc2a7e00b695e15ea6919bf7a752132cbefe3f
Daniil Markov <reedtzhere@gmail.com>
2025-04-27 02:53:23 +0300

    fix bug

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt', 'r') as f:
-    requirements = f.read().splitlines()
+def read_requirements():
+    with open('requirements.txt', 'r') as f:
+        return [line.strip() for line in f if line.strip() and not line.startswith('#')]
 
 setup(
     name="geometry",
     version="0.1.0",
-    install_requires=requirements,
-    packages=find_packages(where='geometry')
+    install_requires=read_requirements(),
+    packages=find_packages(),
+    include_package_data=True
 )
