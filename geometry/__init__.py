8418795a3a9a37285249bd2d17e2473239551282
Daniil Markov <reedtzhere@gmail.com>
2025-04-27 02:06:10 +0300

    fixed bug with installing package

--- geometry/__init__.py
+++ geometry/__init__.py
@@ -1,3 +1,4 @@
 from .area import calculate_circle_area, calculate_triangle_area, calculate_arbitrary_shape_area
 
+__version__ = '0.1.0'
 __all__ = ["calculate_circle_area", "calculate_triangle_area", "calculate_arbitrary_shape_area"]
--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open('requirements.txt', 'r') as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="geometry_calculator",
+    name="geometry",
     version="0.1.0",
     install_requires=requirements,
-    packages=find_packages()
+    packages=find_packages(where='geometry')
 )
