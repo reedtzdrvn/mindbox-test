d27d80641eefe91dfea2062cd0b3d454f1b69cd3
Daniil Markov <reedtzhere@gmail.com>
2025-04-27 01:17:51 +0300

    add reqs.txt && reqs-dev.txt

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="geometry_calculator",
-    version="0.1.0"
+    version="0.1.0",
+    install_requires=requirements,
+    packages=find_packages()
 )
